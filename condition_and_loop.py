44c89a52698a6526bfb7408b63b7abe6c4d88166
Owen-Choi <oldstyle482@gmail.com>
2022-01-19 14:02:09 +0900

    condition and loop

---
+++ condition_and_loop.py
@@ -0,0 +1,23 @@
+print("please enter your age")
+age = int(input("enter here : "))
+if age < 30 :
+    print("such a young man!")
+elif age < 45 :
+    print("such a gentleman!")
+else :
+    print("such an old man!")
+
+for i in [1,2,3,4,5]:
+    print(i)
+
+for k in range(0,5):
+    print(k)
+
+for test in '이게말이돼?' :
+    print(test)
+
+for test2 in ["이게", "말이", "돼?"] :
+    print(test2)
+
+for test3 in range(10,1,-1) :
+    print(test3)
